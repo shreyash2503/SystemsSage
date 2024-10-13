e2d652a87ea998064a7a725e0d29a99e1dcb4bdf
Shreyash Tekade <shreyashtekade2503@gmail.com>
2024-10-13 18:02:34 +0530

    Created embeddings and pushed to supabase

--- src/embeddings.ts
+++ src/embeddings.ts
@@ -1,9 +1,18 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 import { Document } from "@langchain/core/documents";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+require("dotenv").config();
 
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_API_KEY as string;
+const geminiApiKey = process.env.GEMINI_API_KEY as string;
 const pdfPath = process.env.DATA_PATH as string;
 const loader = new PDFLoader(pdfPath);
+const client = createClient(supabaseUrl, supabaseKey);
 
 async function loadPdf() {
   const docs = await loader.load();
@@ -22,7 +31,27 @@ async function splitTextIntoChunks(document: Document<Record<string, any>>[]) {
 }
 
 async function main() {
-  const document = await loadPdf();
-  await splitTextIntoChunks(document);
+  try {
+    const document = await loadPdf();
+    const output = await splitTextIntoChunks(document);
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+      apiKey: geminiApiKey,
+      model: "text-embedding-004",
+      taskType: TaskType.RETRIEVAL_DOCUMENT,
+      title: "System Design",
+    });
+    const vectorStore = await SupabaseVectorStore.fromDocuments(
+      output,
+      embeddings,
+      {
+        client,
+        tableName: "documents",
+      }
+    );
+    console.log(vectorStore);
+    console.log("Vector store created successfully");
+  } catch (error) {
+    console.log(error);
+  }
 }
 main();
